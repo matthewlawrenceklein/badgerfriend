900676d2e793e0dea3d62f7acfe849943de8234b
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-07-22 17:40:32 -0500

    switched db to postgresql as per heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_22_204742) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "badgers", force: :cascade do |t|
     t.string "name"
     t.string "hometown"
